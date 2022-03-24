ec1cf54bb33ff028de4b1be4d9f63cc6c2d81238
123748712 <kemco01@naver.com>
2022-03-24 13:47:45 +0900

    2022.03.24 변수타입 및 연산자

---
+++ day01/myvar.py
@@ -0,0 +1,23 @@
+a = "1"
+b = 1
+c = 1.5
+d = True
+e = False
+# boolean 타입의 첫번째 영문자는 대문자로 표기
+
+# print(d or e)
+# || 와 동일
+
+# print(d and e)
+# && 와 동일
+
+# print(not d)
+# ! 와 동일
+
+# print(b+c)
+
+# print(int(a)+b)
+# 숫자 변환
+
+# print(a+str(1))
+# 문자열 변환
